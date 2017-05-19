e1bc9c1c4a0c6b73fc3e6f541c17e5a7e2617ed7
Rb2030 <rossmail2050@gmail.com>
2017-05-19 10:35:23 +0100

    Initial commit

--- Egg timer/ViewController.swift
+++ Egg timer/ViewController.swift
@@ -9,17 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+     var timer = Timer()
+    
+    
 
+    
+    @IBAction func playButton(_ sender: Any) {
+        
+        timer.invalidate()
+    }
+
+    @IBOutlet weak var countdownLabel: UILabel!
+    
+    func scheduledTimer() {
+        
+        if timer > 0 {
+            countdownLabel.text = "\(timer)"
+            timer -= 1
+    }
+            
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+                super.viewDidLoad()
+        
+      //  timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
